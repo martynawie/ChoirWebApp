780cb06b7dba6b6d52af7286ba6533ac92882db6
becsfranklin <becs.franklin@gmail.com>
2024-03-24 16:44:40 -0400

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -20,3 +20,55 @@ CREATE TABLE member (
 INSERT INTO member (email, fName, lName, password, memberAddress, phoneNumber, dateJoined, birthDate, voice, scheduleDay, is_admin)
 VALUES
     ('admin@mercy.edu', 'Admin', 'Admin', 'Zaq12wsx', 'Mercy', '3473874418', '2024-03-09', '1995-10-26', 'Soprano I', 'Monday', 1);
+
+-- Table for songs
+CREATE TABLE songs (
+    songID VARCHAR(100) PRIMARY KEY,
+    title VARCHAR(100),
+    composer VARCHAR(75),
+);
+
+-- Table for playlists
+CREATE TABLE playlists (
+    playlistID VARCHAR(100) PRIMARY KEY,
+    playlistNumber VARCHAR(20),
+);
+
+-- Table for soloist
+CREATE TABLE soloist (
+    songID VARCHAR(100) FOREIGN KEY, --How do we use a fk?
+    memberID VARCHAR(100) FOREIGN KEY,
+    solo VARCHAR(100),
+);
+
+-- Table for user emergency contact
+CREATE TABLE emergencyInfo (
+    memberID VARCHAR(100) FOREIGN KEY,
+    contactPhone VARCHAR(10),
+    contactfName VARCHAR(100),
+    contactlName VARCHAR (100),
+    contactRelation VARCHAR (30)
+);
+
+-- Table for voice section (do we need this twice?)
+CREATE TABLE songs (
+    sectionID VARCHAR(20) PRIMARY KEY,
+    voice VARCHAR(50) FOREIGN KEY,
+);
+
+-- Table for rehearsal attendance
+CREATE TABLE Attendance (
+    attendanceID VARCHAR(100) PRIMARY KEY,
+    memberID VARCHAR(100) FOREIGN KEY,
+    rehearsalDate VARCHAR(75),
+    attendanceValue BOOLEAN DEFAULT 0,
+    rehearsalLocation VARCHAR(100),
+);
+
+-- Table for events
+CREATE TABLE events (
+    eventID VARCHAR(100) PRIMARY KEY,
+    eventDate VARCHAR(100),
+    eventTime VARCHAR(75),
+    playlistID VARCHAR(100) FOREIGN KEY,
+);
